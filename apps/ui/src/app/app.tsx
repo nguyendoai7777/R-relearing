8e441b101203d9bdb010efe29745ac95b8d5ba1d
Hoa Le <=hoa.lengoc@adamosoft.com>
2022-09-29 10:53:23 +0700

    init

--- apps/ui/src/app/app.tsx
+++ apps/ui/src/app/app.tsx
@@ -1,12 +1,13 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.scss';
-import NxWelcome from './nx-welcome';
+import './app.scss';
 
 export function App() {
   return (
     <>
-      <NxWelcome title="ui" />
-      <div />
+      <img className="most-visited-icon" src="https://qc-static.coccoc.com/a-images/88c/e1c/88ce1c3c7e58c8fe0f517528df8c50d9df42d53e6ad93affb2c6b7b2fd33efad.png" title="Avatar" width="100%" height="100%"/>
+      <img src="https://icdn.24h.com.vn/upload/3-2022/images/2022-09-29//120x90/adt1664419875-b11277bb6e0af94705af6f1826fe76d4.jpg" data-original="https://icdn.24h.com.vn/upload/3-2022/images/2022-09-29//120x90/adt1664419875-b11277bb6e0af94705af6f1826fe76d4.jpg" alt="Vụ cảnh sát đánh người ở Sóc Trăng: Nạn nhân có quyền yêu cầu khởi tố" className="width-100 initial loaded" data-was-processed="true" />
+      <h1 className="my-first">tada</h1>
+      hehe
+      <div/>
     </>
   );
 }
