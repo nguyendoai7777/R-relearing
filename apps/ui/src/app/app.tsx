7cd75d77e26e29ee5ce7c7448b45624ffa4e23ad
Hoa Le <=hoa.lengoc@adamosoft.com>
2022-10-13 11:19:50 +0700

    debug rerender app

--- apps/ui/src/app/app.tsx
+++ apps/ui/src/app/app.tsx
@@ -4,13 +4,20 @@ import { APP_ROUTING } from './app.routing';
 import Navbar from '@cpns/navbar/navbar';
 import RightSidebar from '@cpns/right-sidebar/right-sidebar';
 import LeftSidebar from '@cpns/left-sidebar/left-sidebar';
+import { useAppSelector } from '@store/store';
+import { selectCount } from '@store/slices/counter.slice';
 
 
 export function App() {
+  const count = useAppSelector(selectCount);
   return (
     <>
+
       <div className="d-flex">
         <LeftSidebar/>
+
+
+
         <div className="main">
           <Navbar/>
           <Routes>
@@ -19,6 +26,11 @@ export function App() {
         </div>
         <RightSidebar/>
       </div>
+
+      {/*count.status === 'loading' ? (<h1>loading</h1>) : (<h1>{count.value}</h1>)*/}
+     {/* {count.status === 'loading' && (<h1>loading</h1>) }
+      {count.status !== 'loading' && (<h1>{count.value}</h1>) }*/}
+      <h1>{count.status === 'loading' ? 'loading...' : count.value}</h1>
     </>
   );
 }
